2269825235e5f8893be69974819ff168b31d6e87
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-10-23 12:48:05 +0200

    added copyright to footer

--- partials/footer.php
+++ partials/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="left">
             <span class="secondary">
-                <?php echo "&copy; ". date(Y)." Copyright "."H-FARM."; ?>
+                <?php echo "&copy; ". date(Y)." Copyright H-FARM."; ?>
             </span>
         </div>
     </div>
