fb6b752513bd97d0753845f962eb3fc2f50cfdf6
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-11-16 16:05:52 +0100

    feat(single.php): added basic structure
    Added to single.php the post title and content in order for it to be usable

--- single.php
+++ single.php
@@ -1,3 +1,14 @@
 <?php
-// Create single page
+  get_header();
+  echo "<a href=\"javascript:history.go(-1)\">Back</a>";
 ?>
+
+<h1>
+  <?php single_post_title(); ?>
+</h1>
+
+<main>
+  <?php echo get_the_content(); ?>
+</main>
+
+<?php get_footer(); ?>
