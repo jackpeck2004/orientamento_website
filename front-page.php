aaa20c5e10a98a24d4dff8dd729a6c00497292be
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-12-22 07:43:29 +0100

    added front page

--- front-page.php
+++ front-page.php
@@ -1,10 +1,18 @@
-<div>
-<?php get_header(); ?>
-<h1>
-    <?php the_title(); ?>
-</h1>
-<div class="content">
+<?php wp_head(); ?>
+  <?php require_once('partials/menu.php'); ?>
+
+<main class="home">
+  <div class="front-page">
+    <div class="bg-image">
+      <div class="content">
+        <div class="container">
+          <h1><?php the_title(); ?></h1>
+        </div>
+      </div>
+    </div>
+  <div class="body container text-focus-in">
     <?php the_content(); ?>
-</div>
-</div>
 <?php get_footer(); ?>
+  </div>
+  </div>
+</main>
--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 // Custom Styles
 function orientamento_custom_styles() {
     wp_enqueue_style( 'skeleton-cdn-css', 'https://cdn.jsdelivr.net/npm/skeleton-css@2.0.4/css/skeleton.min.css' );
-    wp_enqueue_style( 'footer-css', get_template_directory_uri() . '/assets/css/footer.css' );
-    wp_enqueue_style( 'menu-css', get_template_directory_uri() . '/assets/css/menu.css' );
+    wp_enqueue_style( 'footer-css', get_template_directory_uri() . '/assets/css/footer.css' ); wp_enqueue_style( 'menu-css', get_template_directory_uri() . '/assets/css/menu.css' );
     wp_enqueue_style( 'archive-css', get_template_directory_uri() . '/assets/css/archive.css' );
     wp_enqueue_style( 'single-css', get_template_directory_uri() . '/assets/css/single.css' );
     wp_enqueue_style( 'hero-css', get_template_directory_uri() . '/assets/css/hero.css' );
+    wp_enqueue_style( 'front-page-css', get_template_directory_uri() . '/assets/css/front-page.css' );
     wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
 }     
 
@@ -23,4 +23,11 @@ function orientamento_custom_menu() {
   );
 }
 add_action( 'init', 'orientamento_custom_menu' );
+
+// Custom background
+$background_args = array(
+    'default-color' => '0000ff',
+    'default-image' => get_template_directory_uri() . '/images/background.jpg',
+);
+add_theme_support( 'custom-background', $background_args );
 ?>
