eddd570fae2bb21904540e6ffdb71ff184118d6f
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-01-10 17:00:07 +0100

    added padding in all pages in order to fit header

--- web/app/themes/orientmento/index.php
+++ web/app/themes/orientmento/index.php
@@ -1,10 +1,10 @@
-<div>
-<?php get_header(); ?>
-<h1>
-    <?php the_title(); ?>
-</h1>
-<div class="content">
-    <?php the_content(); ?>
-</div>
+<div style="margin-top: 85px">
+    <?php get_header(); ?>
+    <h1>
+        <?php the_title(); ?>
+    </h1>
+    <div class="content">
+        <?php the_content(); ?>
+    </div>
 </div>
 <?php get_footer(); ?>
